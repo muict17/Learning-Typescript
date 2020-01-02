7a3183e7df4911f997bd964e8477316f5cb39e5c
lagmanzaza <lagmanmc@gmail.com>
2020-01-02 14:51:01 +0700

    merge

---
+++ Tar/1.data-type.js
@@ -0,0 +1,7 @@
+var a = 100;
+var b = "1000";
+var c = a + b;
+var array = new Map();
+array.set("first", 1);
+array.set("second", 2);
+console.log(array);
---
+++ Tar/1.data-type.ts
@@ -0,0 +1,19 @@
+let a: number = 100;
+const b: string = "1000";
+const c: string = a + b;
+
+let d: any = 100;
+d = "100";
+d = [1, 2, 3, 4];
+d = new Set();
+
+const array: Map<string, number> = new Map();
+array.set("first", 1);
+array.set("second", 2);
+
+console.log(array);
+if (a) {
+  console.log("s");
+} else {
+  console.log("ww");
+}
